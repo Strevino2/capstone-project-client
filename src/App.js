a9e24803fdcac4c3af00b7d5906a956eab9e3f52
Strevino2 <strevino2@hotmail.com>
2022-10-04 01:42:17 -0500

    clearing warnings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import CustomNavigation from './components/CustomNavigation'
-import Navigation from './components/Navigation'
+// import Navigation from './components/Navigation'
 import BottomNavigation from './components/BottomNavigation'
 import "./App.css";
 import Router from "./Router";
