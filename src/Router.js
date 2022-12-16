bb74fd76aec2e137db66897853410f75c77167be
Strevino2 <strevino2@hotmail.com>
2022-12-16 13:28:05 -0600

    edited router files

--- src/Router.js
+++ src/Router.js
@@ -1,35 +1,35 @@
 import React from 'react'
 import { Routes, Route } from 'react-router'
 import Home from './components/Home'
-// import Admin from './components/Admin'
+import Admin from './components/Admin'
 import Menu from './components/Menu'
-// import Login from './components/Login'
-// import cookie from 'cookie'
-// import { Navigate } from "react-router-dom"
+import Login from './components/Login'
+import cookie from 'cookie'
+import { Navigate } from "react-router-dom"
 
 // Write checkAuth function here
-// const checkAuth = () => {
-//     const cookies = cookie.parse(document.cookie);
-//     return cookies["loggedIn"] ? true : false;
-// }
+const checkAuth = () => {
+    const cookies = cookie.parse(document.cookie);
+    return cookies["loggedIn"] ? true : false;
+}
 // Check the cookies for a cookie called "loggedIn"
 
 
 
 // Write ProtectedRoute function here
-// const ProtectedRoute = (props) => {
-//     const {component: Component} = props;
+const ProtectedRoute = (props) => {
+    const {component: Component} = props;
 
-//     return checkAuth() === true ? <Component /> : <Navigate to="/login" />
-// }
+    return checkAuth() === true ? <Component /> : <Navigate to="/login" />
+}
 
 const Router = () => {
     return (
         <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/menu" element={<Menu />} />
-            {/* <Route path="/login" element={<Login />} /> */}
-            {/* <Route path="/admin" element={<ProtectedRoute component={Admin} />} /> */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<ProtectedRoute component={Admin} />} />
         </Routes>
     )
 }
